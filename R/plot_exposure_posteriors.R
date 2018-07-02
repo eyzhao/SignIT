e2f350925293b7e02f63b9e2c475ea9037ae01b5
zhaoez <ezhao15@gmail.com>
2018-07-01 23:17:15 -0700

    Added various unit support to plot exposures posterior functions

--- R/plot_exposure_posteriors.R
+++ R/plot_exposure_posteriors.R
@@ -5,6 +5,8 @@
 #'
 #' @param exposures_mcmc_output     Output from get_exposures.
 #' @param view                      Can be either "violin" or "boxplot".
+#' @param units                     Units to present exposures in. Can be
+#'                                      'mutations', 'megabase', or 'fraction'.
 #'
 #' @return A ggplot plot of the posterior distributions.
 #'
@@ -13,16 +15,30 @@
 #'
 #' @export
 
-plot_exposure_posteriors <- function(exposures_mcmc_output, view = 'violin', signature_trim='Signature') {
+plot_exposure_posteriors <- function(exposures_mcmc_output, view = 'violin', signature_trim='Signature', units='mutations') {
+  if (units == 'megabase') {
+      unit_label = 'Mutations / Mb'
+  } else if (units == 'fraction') {
+      unit_label = 'Exposure Fraction'
+  } else {
+      unit_label = 'Exposure (Mutations)'
+  }
+
   plot <- exposures_mcmc_output$exposure_chain %>%
     mutate(
-        signature = trim_signature_names(signature, signature_trim)
+        signature = trim_signature_names(signature, signature_trim),
+        mutation_burden = sum(exposure),
+        exposure = case_when(
+            units == 'megabase' ~ exposure / 3234.83,
+            units == 'fraction' ~ exposure / exposures_mcmc_output$n_mutations,
+            TRUE ~ exposure
+        )
     ) %>%
     ggplot(aes(
       x = signature %>% as.factor,
       y = exposure
     )) +
-    labs(x = 'Signature', y = 'Exposure') +
+    labs(x = 'Signature', y = unit_label) +
     rotate_x_axis_labels()
     
   if (view == 'boxplot') {
